630651823a5531d51f09547262ffdb8966e73a46
JonJo28 <edgereyx@gmail.com>
2021-11-21 22:05:10 +0100

    first class

--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
-msg = "Hello World"
+# msg = "Hello World"
+# print("Hello World")
+# Variables
+# message = "Hello World, people!"
+# print(message) 
+# message = "U jew!"
+# print (message)
+# Naming Rules
+# Can: Letters, Numbers, Underscores
+message_25 = "25 is here"
+
+# Can't:
+# - Start with numers
+# 2message
+# - No spaces
+# if, for, etc.
+
+# Best Practices
+# - Short but descriptive
+# h_message = "Hello World"
+# Jonas_des = "20_years_old_____a_jew"
+# Jonas_des = "20_years_old_____a_turk"
+# Jonas_des = "20_years_old_____a_jew"
+# print(Jonas_des)
+
+first_name = "jonas".lower()
+# print(first_name) 
+print(first_name).title()
+
+# Can't:
+mm = "2 - 2"
+print(3 + 3)
 
